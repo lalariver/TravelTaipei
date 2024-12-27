4120ddbb100242d9d8d757eed0f679e1fcff58b3
Luke <loveriverain@gmail.com>
2024-12-27 11:37:36 +0800

    Initial Commit

---
+++ TravelTaipei/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TravelTaipei
+//
+//  Created by user on 2024/12/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TravelTaipeiTests/TravelTaipeiTests.swift
@@ -0,0 +1,17 @@
+//
+//  TravelTaipeiTests.swift
+//  TravelTaipeiTests
+//
+//  Created by user on 2024/12/27.
+//
+
+import Testing
+@testable import TravelTaipei
+
+struct TravelTaipeiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
